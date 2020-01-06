eeb5e6a96aa06e34bfa039f2e87952e2bc5eb871
Damian Esteban <damian.esteban@gmail.com>
2020-01-06 01:25:34 -0500

    Update settings

--- org/allRepos.ts
+++ org/allRepos.ts
@@ -4,18 +4,12 @@ import yarn from 'danger-plugin-yarn';
 // NOTE: - The following tasks run on ALL BetterPT repositories.
 
 // Highlight package dependencies
-const highlightDependencies = async () => { await yarn() }
+// const highlightDependencies = async () => { await yarn() }
 
 // All PRs should have a body
-const checkIfPrsHaveBody = () => {
+export default async () => {
   const pr = danger.github.pr;
   if (pr.body.length === null || pr.body.length === 0) {
-    fail('🖍 Please add a description to your PR. No PR is too small to warrant a description.')
+    fail('Please add a description to your PR. No PR is too small to warrant a description.')
   }
-}
-
-
-export default async () => {
-  highlightDependencies();
-  checkIfPrsHaveBody();
 }
