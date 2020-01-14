83f4dff70e6892e86c1e80b954b14e97d0a970a5
Damian Esteban <damian.esteban@gmail.com>
2020-01-14 02:17:14 -0500

    Add hotfix

--- org/allRepos.ts
+++ org/allRepos.ts
@@ -3,14 +3,14 @@ import { danger, warn, fail, GitHubCommit, markdown } from "danger"
 // NOTE: - The following tasks run on ALL BetterPT repositories.
 
 // Valid branch prefixes and names
-const validBranchNames = ['story/', 'bug/', 'task/', 'trivial', 'staging', 'master', 'development'];
+const validBranchNames = ['story/', 'bug/', 'task/', 'hotfix/', 'trivial', 'staging', 'master', 'development'];
 
 const checkBranchPrefix = () => {
   const branchName = danger.github.pr.head.ref;
   const result = validBranchNames.some(value => branchName.includes(value))
   if (result === false) {
     warn(`Merge ref is ${branchName}`);
-    warn('🌲 Your branch name must be of type story, task, trivial, or bug.')
+    warn('🌲 Your branch name must be of type story, task, trivial, bug, or hotfix.')
   }
 }
 
