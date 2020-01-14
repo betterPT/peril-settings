fcb3a3c50059af763a5aef38eecd6d30256af6bf
Damian Esteban <damian.esteban@gmail.com>
2020-01-13 22:28:43 -0500

    Add new rule

--- org/allRepos.ts
+++ org/allRepos.ts
@@ -4,12 +4,16 @@ import yarn from 'danger-plugin-yarn';
 // NOTE: - The following tasks run on ALL BetterPT repositories.
 
 // Highlight package dependencies
-// const highlightDependencies = async () => { await yarn() }
+const highlightDependencies = async () => { await yarn() }
 
-// All PRs should have a body
-export default async () => {
+const checkPrBody = () => {
   const pr = danger.github.pr;
   if (pr.body.length === null || pr.body.length === 0) {
     fail('🖍 Please add a description to your PR. All PRs must have a description.')
   }
+}
+// All PRs should have a body
+export default async () => {
+  highlightDependencies()
+  checkPrBody()
 }
